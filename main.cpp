f6403455cfa66fa3127b20799ba424bd0396c380
Clarissa Soares <clah@ufrn.edu.br>
2016-05-24 10:16:54 -0300

    Leitura de arquivo com dependencias
    Agora, na leitura do arquivo, guarda-se também uma coluna de dependencia de instruções.

--- main.cpp
+++ main.cpp
@@ -4,28 +4,40 @@
 #include <iostream>
 
 #define MAX 10
-
+#define MaxPipelines 8
 using namespace std;
 
 //Estrutura que guarda os componentes de cada instrução:
-//tipooperacao - vetor de caracteres que guarda o tipo de operacao Assembly
+// ordem - recebe a ordem em que se encontra a instrução no bloco de instruções
+//tipoo - vetor de caracteres que guarda o tipo de operacao Assembly
 //receptor - vetor de caracteres do registrados que recebe resultado da operação
 //op1 - vetor de caracteres do registrador operando 1
 //op2 - vetor de caracteres do registrador operando 2
+//depende - mostra um vetor de inteiros que indicam que instruções dependem do receptor da instrução lida
 struct tpInstrucao{
 	int ordem;
-	char tipooperacao[5];
-	char receptor[3];
-	char op1[3];
-	char op2[3];
+	char tipo[5];
+	char receptor[4];
+	char op1[4];
+	char op2[5];
+	int depende[2];
 };
 
+
+struct tpEstagios{
+	int* BI;
+	int* DI;
+	int* EX;
+	int* MEM;
+	int* WB;	
+};
 
 
 
 int main(){
 
-	struct tpInstrucao instrucao[MAX] ;
+	struct tpInstrucao instrucao[MAX];
+
 
 
 	//Inicio: Leitura do arquivo txt
@@ -33,24 +45,35 @@ int main(){
 
 	int qtdePipelines;
 
-	int i=0;
+	int i=0, j=0;
+	int aux;
 
 	if(arquivoInstrucoes.is_open()){
 
 		arquivoInstrucoes >> qtdePipelines;
 
+		if(qtdePipelines > MaxPipelines){
+			cout << "Número de Pipelines superior ao máximo suportado (8)." << endl;
+			return 0;
+		}
+
 
 		//Enquanto nao chega ao final do arquivo, ler e guardar em variaveis
-		while(arquivoInstrucoes >> instrucao[i].ordem >> instrucao[i].tipooperacao >> instrucao[i].receptor >> instrucao[i].op1 >> instrucao[i].op2){			
+		while(arquivoInstrucoes >> instrucao[i].ordem >> instrucao[i].tipo >> instrucao[i].receptor >> instrucao[i].op1 >> 
+			  instrucao[i].op2 >> instrucao[i].depende[0] >> instrucao[i].depende[1]){
 
-			cout << instrucao[i].ordem << " " << instrucao[i].tipooperacao << " " << instrucao[i].receptor << " " << instrucao[i].op1 << " " << instrucao[i].op2 << " " << endl;
+			
+			cout << instrucao[i].ordem << " " << instrucao[i].tipo << " " << instrucao[i].receptor << " " << instrucao[i].op1 << " " << instrucao[i].op2 << " ";
+
+			cout << instrucao[i].depende[0] << "," << instrucao[i].depende[1] << endl;
+
 			i++;
-
 		} 
 		arquivoInstrucoes.close();
 	}	
 		
-	}else{
+	
+	else{
 		cout << "Não foi possivel ler o arquivo" << endl;
 	}
 	//Fim: Leitura do arquivo txt
