d6db43ef94ea326ce688db8b103b684c9f075aa2
Clarissa Soares <clah@ufrn.edu.br>
2016-06-01 00:25:24 -0300

    "Criada a matriz, falta tratar dependências"

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <string.h>
 #include <locale.h>
 #include <iostream>
+#include <vector>
+#include <memory>
+
+using namespace std;
 
 #define MAX 10
 #define MaxPipelines 8
@@ -14,29 +18,33 @@ using namespace std;
 //op1 - vetor de caracteres do registrador operando 1
 //op2 - vetor de caracteres do registrador operando 2
 //depende - mostra um vetor de inteiros que indicam que instruções dependem do receptor da instrução lida
-struct tpInstrucao{
+typedef struct tpInstrucao{
 	int ordem;
 	char tipo[5];
 	char receptor[4];
 	char op1[4];
 	char op2[5];
 	int depende[2];
-};
+	bool naoExecutando = true;
+	int estagio = 0;
+} Instrucao;
 
 
-struct tpEstagios{
+typedef struct tpEstagios{
 	int* BI;
 	int* DI;
 	int* EX;
 	int* MEM;
 	int* WB;	
-};
+} estagios;
 
-
+int pegarInstrucao(vector<Instrucao> &instrucoesRestantes, int inst, int pipe);
+bool existeInstrucoesASeremExecutadas(vector<Instrucao> &instrucoes);
+void liberarInstrucoesExecutadas(vector<Instrucao> &instrucoes);
 
 int main(){
 
-	struct tpInstrucao instrucao[MAX];
+	vector<Instrucao> instrucao;
 
 
 
@@ -44,6 +52,7 @@ int main(){
 	ifstream arquivoInstrucoes ("instrucoes.txt");
 
 	int qtdePipelines;
+	int numeroinstrucoes;
 
 	int i=0, j=0;
 	int aux;
@@ -57,18 +66,29 @@ int main(){
 			return 0;
 		}
 
+		cout << "Número de Pipelines: " << qtdePipelines << endl;
+
+		auto_ptr<Instrucao> inst( new Instrucao);
 
 		//Enquanto nao chega ao final do arquivo, ler e guardar em variaveis
-		while(arquivoInstrucoes >> instrucao[i].ordem >> instrucao[i].tipo >> instrucao[i].receptor >> instrucao[i].op1 >> 
-			  instrucao[i].op2 >> instrucao[i].depende[0] >> instrucao[i].depende[1]){
+		while(arquivoInstrucoes >> inst->ordem >> inst->tipo >> inst->receptor >> inst->op1 >> 
+			  inst->op2 >> inst->depende[0] >> inst->depende[1]){
 
+
+			instrucao.push_back((*inst));
 			
-			cout << instrucao[i].ordem << " " << instrucao[i].tipo << " " << instrucao[i].receptor << " " << instrucao[i].op1 << " " << instrucao[i].op2 << " ";
 
-			cout << instrucao[i].depende[0] << "," << instrucao[i].depende[1] << endl;
+			cout << inst->ordem << " " << inst->tipo << " " << inst->receptor << " " << inst->op1 << " " << inst->op2 << " ";
 
-			i++;
+			cout << inst->depende[0] << "," << inst->depende[1] << endl;
+			inst.release();
+			inst.reset(new Instrucao);
+			
 		} 
+
+		numeroinstrucoes = instrucao.size();
+
+
 		arquivoInstrucoes.close();
 	}	
 		
@@ -78,19 +98,94 @@ int main(){
 	}
 	//Fim: Leitura do arquivo txt
 
-	/*Início: Verificação de Dependências
-	if(){
+	//Início: Verificação de Dependências
+
+	int numeroDoCiclo = 1;
+
+	while( existeInstrucoesASeremExecutadas(instrucao) ){
+		Instrucao aux = instrucao[0];
+
+	//	cout << "1 aux0.estagio: " << aux.estagio << " aux0.naoExecutando" << aux.naoExecutando << endl;
+
+		int estagio[qtdePipelines][5];
+
+		for(int i=0; i<5;i++){
+			for(int j=0;j<qtdePipelines;j++){
+				estagio[i][j] = pegarInstrucao(instrucao,i,j);
+	//			cout << "2 aux0.estagio: " << aux.estagio << " aux0.naoExecutando" << aux.naoExecutando << endl;
+			}
+		}
+
+
+		liberarInstrucoesExecutadas(instrucao);
 		
+		cout << "---------------------------------------------" << endl;
+		cout << "Ciclo " << numeroDoCiclo << endl;
+		for(int i=0;i<qtdePipelines;i++){
+			cout << "Pipeline" << i+1 << "	";		
+		}
+
+		cout << "\n";
+
+		for(int i=0;i<5;i++){
+			for(int j=0; j<qtdePipelines;j++){
+				cout << estagio[i][j] << "		";
+			}
+			cout << "\n";
+		}
+		numeroDoCiclo++;
 	}
 
-	fscanf(*arquivoInstrucoes,%s %s)
-
-
-	//Fim: Verificação de Dependências*/
+	//Fim: Verificação de Dependências
 
 
 
 	//Impressão
+
+	return 0;
+}
+
+
+//Se existir apena uma instrução com estágio menor que 5 retornar verdadeiro, caso contrário retornar falso
+bool existeInstrucoesASeremExecutadas(vector<Instrucao> &instrucoes){
+	
+
+	for(int i=0; i<instrucoes.size(); i++ ){
+		Instrucao* aux = &instrucoes[i];
+
+		if(aux->estagio < 5){
+			return true;
+		}
+		
+	}
+	return false;
+}
+
+//Pegar todas as instruções que estão no estágio naoExecutando para true
+void liberarInstrucoesExecutadas(vector<Instrucao> &instrucoes){
+	for(int i=0; i<instrucoes.size(); i++ ){
+		Instrucao* aux = &instrucoes[i];
+
+		aux->naoExecutando = true;
+	}	
+
+}
+
+int pegarInstrucao(vector<Instrucao> &instrucoesRestantes, int estagio, int pipe){
+	for(int i=0; i<instrucoesRestantes.size(); i++ ){
+		Instrucao *aux = &instrucoesRestantes[i];
+		if(aux->naoExecutando && aux->estagio == estagio){
+			if(true){ // aqui no futuro voce vai substituir pelo algoritmo que vai calcular dependencias
+				aux->naoExecutando = false;
+				aux->estagio++;
+//				cout << "Estagio: " << estagio << " Pipeline:" << pipe;
+//				cout << " Instrucao: " << i<<endl;
+//				cout << "2.1 aux0.estagio: " << aux->estagio << " aux0.naoExecutando" << aux->naoExecutando << endl;
+				return i+1;
+			}
+			
+		}
+	}
 
 	return 0;
 }
